cc1d158f98cb8f0b9a97cae65e2a2897fe94a2c4
10906326 <conde240@gmail.com>
2014-02-10 11:26:31 +0100

    added getter and setter for nom and tonnage

--- src/org/conde/git/Bateau.java
+++ src/org/conde/git/Bateau.java
@@ -9,6 +9,26 @@ public class Bateau{
 	private int tonnage;
 	
 	private List<Bateau> equipage;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getTonnage() {
+		return tonnage;
+	}
+
+	public void setTonnage(int tonnage) {
+		this.tonnage = tonnage;
+	}
+	
+	
+	
+	
 	
 	 
 	
